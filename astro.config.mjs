2c69fc995f23a6620cd60e79b706144a56956e58
Kunal Sharma <kunal.iksm09@gmail.com>
2022-08-29 14:24:09 +0530

    deploying astro project to Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://iksm09.github.io",
+  base: "/portfolio",
   vite: {
     ssr: {
       external: ["svgo"],
